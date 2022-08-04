fd2a0c3ee3c5b19913ce2d836a6491910415959c
bradley <bradleybkempton@gmail.com>
2022-08-03 20:11:13 -0700

    added ExpenseItems comp logic

--- react-test2/src/components/ExpenseItem.js
+++ react-test2/src/components/ExpenseItem.js
@@ -1,21 +1,20 @@
+
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-    const expenseBody = (expenseDate, expenseTitle, expenseAmount) => {
-        return {expenseDate, expenseTitle, expenseAmount}
-      };
-
-    const insuranceValue = expenseBody(Date(2021, 2, 28), "Car Insurance", 294.67)
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
 
   return (
-    <div className="expense-item">
-      <div>{insuranceValue.expenseDate}</div>
-      <div className="expense-item__description">
-        <h2>{insuranceValue.expenseTitle}</h2>
-        <div className="expense-item__price">${insuranceValue.expenseAmount}</div>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
-};
+}
 
 export default ExpenseItem;
