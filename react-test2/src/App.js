3a3a24b06a57cf189483d4d6a8da5d8933bc21be
bradley <bradleybkempton@gmail.com>
2022-09-19 06:53:06 -0700

    add parent-child data sharing using state and event listeners on App.js, NewExpense.js, and ExpenseForm.js.

--- react-test2/src/App.js
+++ react-test2/src/App.js
@@ -25,9 +25,16 @@ function App() {
     },
   ];
 
+
+  const addExpenseHandler = (expense) => {
+    console.log("in App.js");
+    console.log(expense);
+  };
+
+
   return (
     <div className="App">
-       <NewExpense />
+       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items = {expenses}/> 
 
     </div>
