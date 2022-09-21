74d31e5627b9b289aa208a715a5e5b22d5710b62
bradley <bradleybkempton@gmail.com>
2022-09-20 18:37:32 -0700

    added state to expense filter

--- react-test2/src/components/Expenses/Expenses.js
+++ react-test2/src/components/Expenses/Expenses.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
-    
+  const[filteredYear, setFilteredYear] = useState("2020");
+
+  
+    const filterChangeHandler = (selectedYear) => {
+      setFilteredYear(selectedYear);
+    };
+
     return (
+      <div>
         <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
           <ExpenseItem
             title={props.items[0].title}
             amount={props.items[0].amount}
-            date={props.items[0].date}
+            date={props.items[0].date}  
           />    
           <ExpenseItem
             title={props.items[1].title}
@@ -29,6 +39,7 @@ function Expenses(props) {
             date={props.items[3].date}
           /> 
         </Card>
+        </div>
       );
 }
 
